6a925fa4b3951d157b4928957cb46c7ca42ab9ff
Frederic Borrel <frederic.borrel@gmail.com>
2016-09-14 14:33:55 +0200

    Updated FireBase Authentication + Added Sign Out

--- app/src/main/java/com/fredericborrel/atomicrss/view/AuthActivity.java
+++ app/src/main/java/com/fredericborrel/atomicrss/view/AuthActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.fredericborrel.atomicrss.R;
 import com.google.android.gms.auth.api.Auth;
@@ -16,15 +17,26 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
-import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class AuthActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
+    // Constants
     private static final int RC_SIGN_IN = 9001;
     private static final String TAG = "AuthActivity";
 
+    // GoogleAPI and FireBase
     private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    // GUI
     private SignInButton mSignInBtn;
     private ProgressDialog mProgressDialog;
 
@@ -32,9 +44,10 @@ public class AuthActivity extends AppCompatActivity implements GoogleApiClient.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
+        // Configure sign-in to request the user's ID, email address, and basic profile
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         // Build a GoogleApiClient with access to the Google Sign-In API and the
@@ -44,6 +57,23 @@ public class AuthActivity extends AppCompatActivity implements GoogleApiClient.O
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    launchMainActivity();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
         mSignInBtn = (SignInButton)findViewById(R.id.sign_in_button);
         mSignInBtn.setOnClickListener(this);
     }
@@ -51,26 +81,15 @@ public class AuthActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onStart() {
         super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
-        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-        if (opr.isDone()) {
-            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-            // and the GoogleSignInResult will be available instantly.
-            Log.d(TAG, "Got cached sign-in");
-            GoogleSignInResult result = opr.get();
-            handleSignInResult(result);
-        } else {
-            // If the user has not previously signed in on this device or the sign-in has expired,
-            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-            // single sign-on will occur in this branch.
-            showProgressDialog();
-            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                @Override
-                public void onResult(GoogleSignInResult googleSignInResult) {
-                    hideProgressDialog();
-                    handleSignInResult(googleSignInResult);
-                }
-            });
+    @Override
+    protected void onStop() {
+        super.onStop();
+        hideProgressDialog();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 
@@ -101,29 +120,42 @@ public class AuthActivity extends AppCompatActivity implements GoogleApiClient.O
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             Log.d(TAG, "resultCode:" + resultCode);
-            handleSignInResult(result);
+            if(result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
         }
     }
 
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            updateUI(true, acct);
-        } else {
-            // Signed out, show unauthenticated UI.
-            updateUI(false, null);
-        }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        showProgressDialog();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(AuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        hideProgressDialog();
+                    }
+                });
+
     }
 
-    private void updateUI(boolean result, GoogleSignInAccount gsia){
-        if(result){
-            Intent mainActivity = new Intent(this, MainActivity.class);
-            mainActivity.putExtra(MainActivity.GOOGLE_ACCOUNT_KEY, gsia);
-            this.startActivity(mainActivity);
-            finish();
-        }
+    private void launchMainActivity(){
+        Intent mainActivity = new Intent(AuthActivity.this, MainActivity.class);
+        AuthActivity.this.startActivity(mainActivity);
+        finish();
     }
 
     private void showProgressDialog() {
--- app/src/main/java/com/fredericborrel/atomicrss/view/MainActivity.java
+++ app/src/main/java/com/fredericborrel/atomicrss/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fredericborrel.atomicrss.view;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -15,7 +16,8 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.fredericborrel.atomicrss.R;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -25,25 +27,26 @@ import de.hdodenhof.circleimageview.CircleImageView;
  */
 public class MainActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener {
 
-    public static final  String GOOGLE_ACCOUNT_KEY = "GOOGLE_ACCOUNT_KEY";
+    // Constants
     private static final String TAG = "MainActivity";
 
-    private GoogleSignInAccount userAccount;
-    private ActionBarDrawerToggle drawerToggle;
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-    private TextView userNameMenu;
-    private TextView emailMenu;
-    private CircleImageView profilePictureMenu;
+    // FireBase
+    private FirebaseUser mUser;
+
+    // GUI
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private TextView mUserNameMenu;
+    private TextView mEmailMenu;
+    private CircleImageView mProfilePictureMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get Google Account Information
-        Bundle b = getIntent().getExtras();
-        userAccount = b.getParcelable(GOOGLE_ACCOUNT_KEY);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
 
         // Setup the drawer menu
         setupDrawer();
@@ -72,14 +75,14 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
+        mDrawerToggle.syncState();
         shouldDisplayHomeUp();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
         shouldDisplayHomeUp();
     }
 
@@ -90,7 +93,7 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
 
     private void shouldDisplayHomeUp(){
         boolean canGoBack = getSupportFragmentManager().getBackStackEntryCount() > 0;
-        drawerToggle.setDrawerIndicatorEnabled(!canGoBack);
+        mDrawerToggle.setDrawerIndicatorEnabled(!canGoBack);
     }
 
     @Override
@@ -102,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Activate the navigation drawer toggle
-        if (drawerToggle.onOptionsItemSelected(item)) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -110,16 +113,16 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
 
     private void setupDrawer(){
         // Retrieve Views
-        drawerLayout = (DrawerLayout)findViewById(R.id.main_drawer_layout);
-        navigationView = (NavigationView)findViewById(R.id.main_navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.main_drawer_layout);
+        mNavigationView = (NavigationView)findViewById(R.id.main_navList);
 
-        View header = navigationView.getHeaderView(0);
-        userNameMenu = (TextView)header.findViewById(R.id.menu_username);
-        emailMenu = (TextView)header.findViewById(R.id.menu_email);
-        profilePictureMenu = (CircleImageView)header.findViewById(R.id.menu_image);
+        View header = mNavigationView.getHeaderView(0);
+        mUserNameMenu = (TextView)header.findViewById(R.id.menu_username);
+        mEmailMenu = (TextView)header.findViewById(R.id.menu_email);
+        mProfilePictureMenu = (CircleImageView)header.findViewById(R.id.menu_image);
 
         // Adding behavior when we are opening/closing the drawer
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_open, R.string.menu_close){
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.menu_open, R.string.menu_close){
             // Called when a drawer has settled in a completely open state.
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
@@ -132,23 +135,35 @@ public class MainActivity extends AppCompatActivity implements FragmentManager.O
                 invalidateOptionsMenu();
             }
         };
-        drawerToggle.setDrawerIndicatorEnabled(true);
-        drawerLayout.addDrawerListener(drawerToggle);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
 
-        navigationView.setNavigationItemSelectedListener(
+        mNavigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         item.setChecked(true);
-                        drawerLayout.closeDrawers();
+                        mDrawerLayout.closeDrawers();
+                        switch (item.getItemId()){
+                            case R.id.sign_out_menu_item :
+                                signOut();
+                        }
                         return true;
                     }
                 }
         );
 
         // Set Google Account Information to the header
-        emailMenu.setText(userAccount.getEmail());
-        userNameMenu.setText(userAccount.getDisplayName());
-        Picasso.with(getApplicationContext()).load(userAccount.getPhotoUrl()).into(profilePictureMenu);
+        mEmailMenu.setText(mUser.getEmail());
+        mUserNameMenu.setText(mUser.getDisplayName());
+        Picasso.with(getApplicationContext()).load(mUser.getPhotoUrl()).into(mProfilePictureMenu);
+    }
+
+    // Sign out the user and send him back to the Authentication Activity
+    private void signOut(){
+        FirebaseAuth.getInstance().signOut();
+        Intent authActivity = new Intent(MainActivity.this, AuthActivity.class);
+        MainActivity.this.startActivity(authActivity);
+        finish();
     }
 }
